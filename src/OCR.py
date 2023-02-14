118415a8f362cfc65ab6cc62f7a7200fdfeb6ef4
seif7666 <seifeldin7666@gmail.com>
2023-02-14 09:25:40 +0000

    Debugging

--- src/OCR.py
+++ src/OCR.py
@@ -57,6 +57,7 @@ def run(image_path):
 
     exc_time = after-before
     # Create file with the same name of the image
+    print(f'Image path is {image_path}')
     img_name = image_path.split('\\')[1].split('.')[0]
 
     with open(f'output/text/{img_name}.txt', 'w', encoding='utf8') as fo:
